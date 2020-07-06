f0f7f548e0e7e189a190bede953aaff2b09bf9fa
kartiknavpute <kartiknavpute4444@gmail.com>
2020-07-06 18:47:37 +0530

    this is for beginners

---
+++ perimeter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	float l,area,perimeter;
+	printf("/n Enter a length=");
+	scanf("%f",&l);
+	area=l*l;
+	perimeter=4*l;
+	printf("Area is%f & perimeter is %f",area,perimeter);
+}
