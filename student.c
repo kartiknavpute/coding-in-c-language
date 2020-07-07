5fda2998d37fedc256cc5d92b1c7b2f691894b43
Your Name <youremail@domain.com>
2020-07-07 10:33:09 +0530

    result declaration

---
+++ student.c
@@ -0,0 +1,19 @@
+//written by kartik
+#include<stdio.h>
+void main()
+{
+	int n,i;
+	printf("Enter a value of n=");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		int a,m,cp,chem,mec;
+		char b;
+		printf("\nEnter student name:");
+		scanf("%ch",&b);
+		printf("\n Roll no:");
+		scanf("%d",&a);
+		printf("\nEnter a Marks:maths,cp,chemistry,mechanics respectively.");
+		scanf("%d%d%d%d",&m,&cp,&chem,&mec);
+	}
+}
