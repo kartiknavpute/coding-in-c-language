13918579b5f6c7bd5ef4463dbc930d3b29eb8cce
Your Name <youremail@domain.com>
2020-07-07 13:10:29 +0530

    to get palendrone number

---
+++ palendrone.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int n,a,b,d,sum=0,s;
+	printf("enter a number=");
+	scanf("%d",&a);
+	n=a;
+	for(a!=0)
+	{
+		b=a%10;
+		d=sum*10+b;
+		s=a*10;
+	}
+	if(n=a)
+	printf("the number is palendrone");
+	else
+	printf("the number is not palendrone");
+}
+
